c1ec42db651cce71e86c5bab6de5433e3f65be3c
joanamds <joanamdsantos1@gmail.com>
2023-03-27 11:31:05 -0300

    :white_check_mark: test: Requisito 10 finalizado, PVP implementado

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,13 +1,29 @@
 import Character from '../Character';
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
 export default class PVP extends Battle {
-  player1: Character;
-  player2: Character;
+  player1: Character | Fighter;
+  player2: Character | Fighter;
 
-  constructor(p1: Character, p2: Character) {
+  constructor(p1: Character | Fighter, p2: Character | Fighter) {
     super(p1);
     this.player1 = p1;
     this.player2 = p2;
+  }
+
+  fight(): number {
+    do {
+      this.player1.attack(this.player2);
+      if (this.player2.lifePoints > 0) {
+        this.player2.attack(this.player1);
+      }
+    }
+    while (this.player1.lifePoints > 0 && this.player2.lifePoints > 0);
+
+    if (this.player1.lifePoints > this.player2.lifePoints) {
+      return 1;
+    }
+    return -1;
   }
 }
